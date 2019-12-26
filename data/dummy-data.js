d7731c9ad81d75dfeea01ecb64194c4281d9dd73
mansourlotfi <mansourlotfi@gmail.com>
2019-12-26 21:41:07 +0330

    styling product item

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -4,7 +4,7 @@ const PRODUCTS = [
 		'p1',
 		'u1',
 		'Red Shirt',
-		'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+		'https://www.iranpooshak.com/wp-content/uploads/2019/07/x-1-600x600.jpg',
 		'A red t-shirt, perfect for days with non-red weather.',
 		29.99
 	),
@@ -12,7 +12,7 @@ const PRODUCTS = [
 		'p2',
 		'u1',
 		'Blue Carpet',
-		'https://images.pexels.com/photos/6292/blue-pattern-texture-macro.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+		'https://kashansara.com/wp-content/uploads/2018/11/FarshKashan-Eli-T-1000shane-3000P-3.jpg',
 		'Fits your red shirt perfectly. To stand on. Not to wear it.',
 		99.99
 	),
@@ -20,7 +20,7 @@ const PRODUCTS = [
 		'p3',
 		'u2',
 		'Coffee Mug',
-		'https://images.pexels.com/photos/160834/coffee-cup-and-saucer-black-coffee-loose-coffee-beans-160834.jpeg?cs=srgb&dl=bean-beans-black-coffee-160834.jpg&fm=jpg',
+		'http://www.lottos.ir/content/images/thumbs/0003104_-.jpeg',
 		'Can also be used for tea!',
 		8.99
 	),
@@ -28,23 +28,23 @@ const PRODUCTS = [
 		'p4',
 		'u3',
 		'The Book - Limited Edition',
-		'https://images.pexels.com/photos/46274/pexels-photo-46274.jpeg?cs=srgb&dl=blur-blurred-book-pages-46274.jpg&fm=jpg',
+		'https://seemorgh.com/images/contenet1398/culture/04/2/book.jpg',
 		"What the content is? Why would that matter? It's a limited edition!",
 		15.99
 	),
 	new Product(
 		'p5',
 		'u3',
-		'PowerBook',
-		'https://get.pxhere.com/photo/laptop-computer-macbook-mac-screen-water-board-keyboard-technology-air-mouse-photo-airport-aircraft-tablet-aviation-office-black-monitor-keys-graphic-hardware-image-pc-exhibition-multimedia-calculator-vector-water-cooling-floppy-disk-phased-out-desktop-computer-netbook-personal-computer-computer-monitor-electronic-device-computer-hardware-display-device-448748.jpg',
+		'notebook',
+		'https://www.asuscenter.ir/images/Marketing/%D8%B1%D8%A7%D9%87%D9%86%D9%85%D8%A7%DB%8C%20%D8%AE%D8%B1%DB%8C%D8%AF/%D9%86%D8%AA%20%D8%A8%D9%88%DA%A9/%D9%86%D8%AA-%D8%A8%D9%88%DA%A9--1.jpg',
 		'Awesome hardware, crappy keyboard and a hefty price. Buy now before a new one is released!',
 		2299.99
 	),
 	new Product(
 		'p6',
 		'u1',
-		'Pen & Paper',
-		'https://cdn.pixabay.com/photo/2015/10/03/02/14/pen-969298_1280.jpg',
+		'Pen',
+		'https://aragifts.com/uploads/product/1518225893.jpg',
 		"Can be used for role-playing (not the kind of role-playing you're thinking about...).",
 		5.49
 	)
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { FlatList, Text } from 'react-native';
 import { useSelector } from 'react-redux';
+import ProductItem from '../../components/shop/ProductItem';
 
 const ProductsOverviewScreen = (props) => {
 	const products = useSelector((state) => state.products.availableProducts);
-	return <FlatList data={products} renderItem={(itemData) => <Text> {itemData.item.title} </Text>} />;
+	return (
+		<FlatList
+			data={products}
+			renderItem={(itemData) => (
+				<ProductItem
+					image={itemData.item.imageUrl}
+					title={itemData.item.title}
+					price={itemData.item.price}
+					onViewDetaile={() => {}}
+					onAddToCart={() => {}}
+				/>
+			)}
+		/>
+	);
 };
 ProductsOverviewScreen.navigationOptions = {
 	headerTitle: 'All fucking Products'
