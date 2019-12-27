b15b5935e088a555c01f61b0fdb7df4ac5c33355
mansourlotfi <mansourlotfi@gmail.com>
2019-12-28 00:07:25 +0330

    header icon

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList, Platform } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
+import { HeaderButtons, item, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../../components/UI/HeaderButton';
 
 const ProductsOverviewScreen = (props) => {
 	const products = useSelector((state) => state.products.availableProducts);
@@ -30,6 +32,11 @@ const ProductsOverviewScreen = (props) => {
 	);
 };
 ProductsOverviewScreen.navigationOptions = {
-	headerTitle: 'All fucking Products'
+	headerTitle: 'All Products',
+	headerRight: (
+		<HeaderButtons HeaderButtonComponent={HeaderButton}>
+			<Item title="Cart" iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'} onPress={() => {}} />
+		</HeaderButtons>
+	)
 };
 export default ProductsOverviewScreen;
